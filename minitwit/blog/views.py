dfc9093c6c0c269cb237d9e970b034827f100d2e
Huang <CWR.CHAO.HUANG@newcorp.com>
2016-08-29 14:49:25 -0400

    add pagination

--- minitwit/blog/views.py
+++ minitwit/blog/views.py
@@ -1,10 +1,24 @@
 from werkzeug import check_password_hash, generate_password_hash
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import User, Follower, Message
 from datetime import datetime
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib import messages
+from .models import User, Follower, Message
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+
+def common_timeline(request, posts):
+    paginator = Paginator(posts, 5) # Show 25 contacts per page
+    page = request.GET.get('page')
+    try:
+        paged_posts = paginator.page(page)
+    except PageNotAnInteger:
+        paged_posts = paginator.page(1)
+    except EmptyPage:
+        paged_posts = paginator.page(paginator.num_pages)
+    return render(request, 'timeline.html', {'posts': paged_posts})
 
 
 def timeline(request):
@@ -15,20 +29,20 @@ def timeline(request):
     current_user = User.objects.get(pk=current_user_id)
     following_ids = current_user.follower_set.values_list('whom', flat=True)
     all_ids = list(following_ids) + [current_user_id]
-    posts = Message.objects.filter(author_id__in=all_ids).order_by('-pub_date')[:20]
-    return render(request, 'timeline.html', {'posts': posts})
+    posts = Message.objects.filter(author_id__in=all_ids).order_by('-pub_date')
+    return common_timeline(request, posts)
 
 
 def public_timeline(request):
-    latest_posts = Message.objects.order_by('-pub_date')[:20]
-    return render(request, 'timeline.html', {'posts': latest_posts})
+    latest_posts = Message.objects.order_by('-pub_date')
+    return common_timeline(request, latest_posts)
 
 
 def user_timeline(request, username):
     try:
         profile_user = User.objects.get(username=username)
     except ObjectDoesNotExist:
-        return HttpResponse('This user does not ddfdfexist', status=401)
+        return HttpResponse('This user does not exist', status=401)
     posts = profile_user.message_set.order_by('-pub_date')[:20]
     current_user_id = request.session.get('user_id')
     is_followed = False
@@ -37,7 +51,6 @@ def user_timeline(request, username):
     is_same_user = False
     if current_user_id and profile_user.pk == current_user_id:
         is_same_user = True
-    # change the messages both here and the templates to posts
     return render(request, 'timeline.html', {'posts': posts, 'profile_user': profile_user, 'followed': is_followed,
                                              'same_user': is_same_user})
 
