bf82d036704799f3545f927a36df89341334bf0f
chao huang <chao huang>
2016-09-11 19:31:38 -0400

    refactor

---
+++ cachestudy/myapp/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.core.cache import cache
+from io import BytesIO
+from PIL import Image, ImageDraw
+
+
+class ImageForm(forms.Form):
+    """Form to validate requested placeholder image."""
+
+    height = forms.IntegerField(min_value=1, max_value=2000)
+    width = forms.IntegerField(min_value=1, max_value=2000)
+
+    def generate(self, image_format='PNG'):
+        """Generate an image of the given type and return as raw bytes."""
+        height = self.cleaned_data['height']
+        width = self.cleaned_data['width']
+        key = '{}.{}.{}'.format(width, height, image_format)
+        content = cache.get(key)
+        if content is None:
+            image = Image.new('RGB', (width, height))
+            draw = ImageDraw.Draw(image)
+            text = '{} X {}'.format(width, height)
+            textwidth, textheight = draw.textsize(text)
+            if textwidth < width and textheight < height:
+                texttop = (height - textheight) // 2
+                textleft = (width - textwidth) // 2
+                draw.text((textleft, texttop), text, fill=(255, 255, 255))
+            content = BytesIO()
+            image.save(content, image_format)
+            content.seek(0)
+            cache.set(key, content, 60 * 60)
+        return content
---
+++ cachestudy/myapp/urls.py
@@ -0,0 +1,9 @@
+
+from django.conf.urls import url
+from .views import placeholder, index
+
+urlpatterns = (
+    url(r'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder,
+        name='placeholder'),
+    url(r'^$', index, name='homepage'),
+)
