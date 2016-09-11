6f01a48b38b3f4521cd56d25fef50d36bd57349e
Chao Huang <hchao8@gmail.com>
2016-09-10 20:26:06 -0400

    Update settings.py

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -6,14 +6,14 @@ from __future__ import absolute_import
 
 # Celery settings
 
-BROKER_URL = 'amqp://test:test@localhost//'
+BROKER_URL = 'redis://localhost:6379/0'
 
 #: Only add pickle to this list if your broker is secured
 #: from unwanted access (see userguide/security.html)
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_RESULT_BACKEND = "amqp"
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 
 # Django settings for proj project.
