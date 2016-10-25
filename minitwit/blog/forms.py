f4609ed5109ec199490908dc383a41c540f957cf
Chao Huang <hchao8@gmail.com>
2016-10-25 16:54:34 -0400

    Update forms.py

--- minitwit/blog/forms.py
+++ minitwit/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate
 
 
 class UserForm(UserCreationForm):
