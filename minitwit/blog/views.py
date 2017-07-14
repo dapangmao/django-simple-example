4748741f9bedbcad9824807c51d4775bc4ddb36c
Chao Huang <hchao8@gmail.com>
2017-07-14 14:56:32 -0400

    more

--- minitwit/blog/views.py
+++ minitwit/blog/views.py
@@ -1,13 +1,13 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import Http404
-from django.core.exceptions import ObjectDoesNotExist
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout
+from django.views import View
 
-from .models import Follower, Message
+from .models import Message
 from .forms import UserForm, LoginForm
 
 
@@ -23,36 +23,38 @@ def get_paged_posts(request, posts, n=5):
     return paged_posts
 
 
-def timeline(request):
-    if not request.user.is_authenticated():
-        return public_timeline(request)
-    followed_users = request.user.follower_set.values_list('followed', flat=True)
-    all_ids = list(followed_users) + [request.user]
-    posts = Message.objects.filter(author__in=all_ids).order_by('-pub_date')
-    paged_posts = get_paged_posts(request, posts)
-    return render(request, 'timeline.html', {'posts': paged_posts})
+class MyTimeline(LoginRequiredMixin, View):
+    login_url = '/public'
+    redirect_field_name = ''
+
+    def get(self, request):
+        followed_users = request.user.follower_set.values_list('followed', flat=True)
+        all_users = list(followed_users) + [request.user]
+        posts = Message.objects.filter(author__in=all_users).order_by('-pub_date')
+        paged_posts = get_paged_posts(request, posts)
+        return render(request, 'timeline.html', {'posts': paged_posts})
+
+    def post(self, request):
+        if request.POST.get('text'):
+            request.user.message_set.create(text=request.POST.get('text'))
+        return self.get(request)
 
 
 def public_timeline(request):
-    latest_posts = Message.objects.order_by('-pub_date')
-    paged_posts = get_paged_posts(request, latest_posts)
+    posts = Message.objects.order_by('-pub_date')
+    paged_posts = get_paged_posts(request, posts)
     return render(request, 'timeline.html', {'posts': paged_posts})
 
 
 def user_timeline(request, username):
     profile_user = get_object_or_404(User, username=username)
     profile_user_posts = profile_user.message_set.order_by('-pub_date')
-    is_followed, is_same_user = False, False
-    if request.user.is_authenticated():
-        current_user_id = request.user.pk
-        if request.user.follower_set.filter(followed=profile_user).exists():
-            is_followed = True
-        if profile_user.pk == current_user_id:
-            is_same_user = True
+    is_followed = False
+    if hasattr(request.user, 'follower_set') and request.user.follower_set.filter(followed=profile_user).exists():
+        is_followed = True
     paged_posts = get_paged_posts(request, profile_user_posts)
     return render(request, 'timeline.html',
-                  {'posts': paged_posts, 'profile_user': profile_user, 'followed': is_followed,
-                   'same_user': is_same_user})
+                  {'posts': paged_posts, 'profile_user': profile_user, 'followed': is_followed})
 
 
 @login_required
@@ -67,13 +69,6 @@ def unfollow_user(request, username):
     followed = get_object_or_404(User, username=username)
     request.user.follower_set.filter(followed=followed).delete()
     return redirect('user_timeline', username=username)
-
-
-@login_required
-def add_message(request):
-    if request.POST.get('text'):
-        request.user.message_set.create(text=request.POST.get('text'))
-    return redirect('timeline')
 
 
 def login_view(request):
--- minitwit/minitwit/urls.py
+++ minitwit/minitwit/urls.py
@@ -4,11 +4,10 @@ from django.contrib import admin
 from blog import views
 
 urlpatterns = [
-    url(r'^$', views.timeline, name='timeline'),
+    url(r'^$', views.MyTimeline.as_view(), name='timeline'),
     url(r'public/', views.public_timeline, name='public_timeline'),
     url(r'^user/(?P<username>\w+)/unfollow/', views.unfollow_user, name='unfollow_user'),
     url(r'^user/(?P<username>\w+)/follow/', views.follow_user, name='follow_user'),
-    url(r'^add_message/$', views.add_message, name='add_message'),
     url(r'^logout/$', views.logout_view, name='logout_view'),
     url(r'^login/$', views.login_view, name='login_view'),
     url(r'^register/$', views.register, name='register'),
